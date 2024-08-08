50c2b70957a1340d5e5b62089b0d34e6841afa17
hykim02 <hykim4435@naver.com>
2024-08-08 22:39:32 +0900

    server implement

--- server.py
+++ server.py
@@ -2,8 +2,9 @@
 
 import socket
 import struct
-
 import pickle
+import torch
+import torchvision.models as models
 
 # for demonstration
 import matplotlib.pyplot as plt
@@ -11,14 +12,45 @@ import matplotlib.pyplot as plt
 HOST = '0.0.0.0'
 PORT = 1234
 
+def load_model():
+    # load ai model
+
+    # model = TheModelClass(*args, **kwargs)
+    # model.load_state_dict(torch.load(PATH))
+    # model.eval() // 예측 모드
+    pass
+
+# img 모델에 맞게 전처리
 def process_image(image):
+    model = load_model()
     # you would do your processing here and return some results in whatever format you need
     print("Processing image")
     # plot the image for demonstration
-    # plt.imshow(image)
-    return "testresult"
+    plt.imshow(image)
+
+     # 예: result = model.predict(image)
+    result = model.predict(image)  # 이 부분을 실제 모델 예측 결과로 교체
+    
+    # 모델의 예측값은 3가지 -> 경우의 수에 따라 예측값과 정수값을 어떻게 매핑할지 ?
+    # 결과값을 정수로 변환
+    result_map = {
+        "stop": 0,
+        "turn-right": 1,
+        "turn-left": 2,
+        "straight": 3,
+        "stop & turn-right": 4,
+        "stop & turn-left": 5,
+        "stop & straight": 6
+    }
+
+    int_result = result_map.get(result, 3)  # 결과값이 맵에 없으면 3으로 설정
+    return int_result
+    
 
 def main():
+    # load model
+    model = load_model()
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen(1)
@@ -52,10 +84,14 @@ def main():
                 # process the image
                 predictions = process_image(frame_data)
 
+
+                # stop: 0, turn-right: 1, turn-left: 2, straight: 3, 
+                # stop & turn-right: 4, stop & turn-left: 5, stop & straight: 6
                 # send the results back
                 predictions = pickle.dumps(predictions)
                 client_socket.sendall(struct.pack("L", len(predictions)) + predictions)
                 print("Returned")
+                
         except Exception as e:
             print(e)
         finally:
